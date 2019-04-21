f29d677c8859e3ab8ef48d2d7b154c17679aef34
hypehuman <hypehuman@users.noreply.github.com>
2019-04-22 01:46:23 +0200

    Increase the range

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,84 +1,62 @@
 package com.example.examplemod;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.GuiOverlayDebug;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.RayTraceFluidMode;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
 public class ExampleMod
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static double looking_at_distance = 200; // default 20
 
     public ExampleMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
+    public void onPostOverlayRender(RenderGameOverlayEvent.Post event) {
+        if (event.getType() != ElementType.DEBUG) {
+            return;
+        }
+
+        GuiIngame guiIngame = Minecraft.getInstance().ingameGUI;
+        GuiOverlayDebug overlayDebug = getOverlayDebug(guiIngame);
+
+        // Override what was done in net.minecraftforge.client.GuiIngameForge.GuiOverlayDebugForge.update()
+        Entity entity = Minecraft.getInstance().getRenderViewEntity();
+        setTraceBlock(overlayDebug, entity.rayTrace(looking_at_distance, 0.0F, RayTraceFluidMode.NEVER));
+        setTraceFluid(overlayDebug, entity.rayTrace(looking_at_distance, 0.0F, RayTraceFluidMode.ALWAYS));
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
+    /*
+     * Access protected field GuiOverlayDebug.overlayDebug
+     */
+    private static GuiOverlayDebug getOverlayDebug(GuiIngame instance) {
+        return ObfuscationReflectionHelper.getPrivateValue(GuiIngame.class, instance, "field_175198_t");
+    }
+
+    /*
+     * Mutate private field GuiOverlayDebug.rayTraceBlock
+     */
+    private static void setTraceBlock(GuiOverlayDebug overlayDebug, RayTraceResult value) {
+        ObfuscationReflectionHelper.setPrivateValue(GuiOverlayDebug.class, overlayDebug, value, "field_211537_g");
+    }
+
+    /*
+     * Mutate private field GuiOverlayDebug.rayTraceFluid
+     */
+    private static void setTraceFluid(GuiOverlayDebug overlayDebug, RayTraceResult value) {
+        ObfuscationReflectionHelper.setPrivateValue(GuiOverlayDebug.class, overlayDebug, value, "field_211538_h");
     }
 }
